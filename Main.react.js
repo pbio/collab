3eaae7a098ed4f40891cde9c2b93197a15886364
Paul Bioche <pebioche@gmail.com>
2016-09-24 18:41:19 +0200

    initial commit, untested

---
+++ Main.react.js
@@ -0,0 +1,16 @@
+//Main Component
+
+var React = require('react');								
+var ReactDOM=require('react-dom');
+
+var Main = React.createClass({
+	getInitialState: function() {
+		return {};
+	},
+	
+	render: function(){
+    	//Return content
+		return <div>HelloWorld</div>;
+	}
+});
+module.exports = Main;
---
+++ myApp.js
@@ -0,0 +1,11 @@
+//This is the top level component
+var React = require('react');
+var ReactDOM=require('react-dom');
+
+ReactDOM.render(
+	(<Main/>), 
+	document.getElementById('myApp')
+);
+	
+
+	
