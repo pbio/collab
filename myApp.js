20bea506408067ac72ed78a92aa874358e24dd39
Paul Bioche <pebioche@gmail.com>
2016-09-24 19:01:26 +0200

    fixed require, dependencies etc

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 //This is the top level component
 var React = require('react');
 var ReactDOM=require('react-dom');
+var Main = require('./components/Main.react.js');
 
 ReactDOM.render(
-	(<Main/>), 
+	<Main />, 
 	document.getElementById('myApp')
 );
 	
